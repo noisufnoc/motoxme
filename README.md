d966072a74ef9ce8d2236a8f146e88cbb42e227e
Mike Walker <noisufnoc@gmail.com>
2013-11-29 18:21:57 -0800

    initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+motoxme
+=======
+
+I want a MotoX on Cyber Monday. Check the price and let me know.
---
+++ xmebro.py
@@ -0,0 +1,19 @@
+__author__ = 'miwalker'
+
+import time
+import urllib
+from BeautifulSoup import BeautifulSoup
+
+busta = str(time.time()).split('.')[0]
+
+url = 'http://www.motorola.com/us/shop-all-mobile-phones-1/Moto-X-Developer-Edition-Verizon/moto-x-developer-edition-verizon.html?v=%s' % busta
+
+soup = BeautifulSoup(urllib.urlopen(url))
+
+price = str(soup('p', 'price')[0]).split('>')[2].split('<')[0]
+
+if price == '$549.99':
+    print 'NO SALE %s' % price
+else:
+    print 'SALE! %s' % price
+
